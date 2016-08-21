8f6d99ba2969e5eed2f757f067ba73073f23c9d6
tedavid93 <tedavid93@gmail.com>
2016-08-20 22:20:37 -0400

    Add files via upload

---
+++ Website.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Untitled Document</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<style>
+.button {
+	display: inline-block;
+	width:23%;
+	height:50px;
+	float: left;
+	border:none;
+	border-radius:5px;
+	background-color:white;
+	margin:1%;
+}
+
+.button:hover {
+	border:solid thin #000000;
+}
+
+.button:active {
+	border:solid thin #000000;
+	background-color:#A3A3A3;
+}
+.button:visited {
+	border:none;
+}
+
+.button-group {
+	height:50px;
+	max-width:100%;
+	width:100&;
+	vertical-align:middle;
+}
+
+.jumbotron {
+	background-color:#323232;
+	width:100%;
+}
+
+#name {
+	letter-spacing:10px;
+	color:white;
+	font-family:Cambria, "Hoefler Text", "Liberation Serif", Times, "Times New Roman", serif;
+	font-size:48px;
+	font-weight:100;
+}
+</style>
+  </head>
+  <body width="100%">
+<div class="container">
+ <div class="jumbotron text-center">
+	<p id="name">TYLER EDWARD DAVID</p>
+ </div>
+	<div class="button-group">
+		<button type="button" class="button">Home</button>
+    	<button type="button" class="button">About</button>
+    	<button type="button" class="button">Portfolio</button>
+    	<button type="button" class="button">Resume</button>
+	</div>
+ </div>
+  </body>
+</html>
